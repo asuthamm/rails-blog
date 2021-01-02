c1e6fa9988d74155632da3228a46b1b8575e19d8
Alex Suthammanont <asuthamm@gmail.com>
2021-01-01 22:30:31 -0500

    add new & create to article

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get '/articles', to: 'articles#index'
-  root 'articles#index'
-  get '/articles/:id', to: 'articles#show'
 
+  root 'articles#index'
   resources :articles
+
+  # get 'articles/index'
+  # get '/articles', to: 'articles#index'
+
+  # get '/articles/:id', to: 'articles#show'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
